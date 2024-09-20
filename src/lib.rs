0b28401c81a1236b7b4474edf43e87603c8113ae
Matt <zedddiezxc@gmail.com>
2024-09-20 15:10:36 +0200

    Form data deserialization with serde

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,10 @@
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use actix_web::dev::Server;
+use actix_web::{web, App, HttpResponse, HttpServer};
 use std::net::TcpListener;
-async fn health_check() -> impl Responder {
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
@@ -9,8 +12,15 @@ pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
     })
         .listen(listener)?
         .run();
     Ok(server)
+}
+
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String
 }
