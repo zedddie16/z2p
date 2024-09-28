aa4945923a57083072a2221a29e9e4a7f12d01fe
Matt <zedddiezxc@gmail.com>
2024-09-28 17:54:59 +0200

    before test isolation

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,17 @@
 use std::net::TcpListener;
-use sqlx::{Connection, PgConnection};
+use sqlx::{Connection, PgPool};
 use zero2prod::startup::run;
 use zero2prod::configuration::get_configuration;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection = PgConnection::connect(
+    let connection_pool = PgPool::connect(
             &configuration.database.connection_string()
         )
         .await
         .expect("Failed to connect to Postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener, connection)?.await
+    run(listener, connection_pool)?.await
 }
