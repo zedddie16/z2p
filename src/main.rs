6af488895c37887ddec8a7d388a914e1d381a30f
Matt <zedddiezxc@gmail.com>
2024-09-28 16:47:56 +0200

    actix-web workers

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
 use std::net::TcpListener;
+use sqlx::{Connection, PgConnection};
 use zero2prod::startup::run;
 use zero2prod::configuration::get_configuration;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    // Panic if we can't read configuration
     let configuration = get_configuration().expect("Failed to read configuration.");
-    // No more hard-coded `8000` - it's now coming from our settings!
+    let connection = PgConnection::connect(
+            &configuration.database.connection_string()
+        )
+        .await
+        .expect("Failed to connect to Postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener)?.await
+    run(listener, connection)?.await
 }
