01f36e9799c8b9032b4b2424fa8f3f0002386a74
Matt <zedddiezxc@gmail.com>
2024-10-02 14:25:14 +0200

    tracing

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -4,6 +4,7 @@ use sqlx::types::chrono;
 use chrono::Utc;
 use uuid::Uuid;
 
+
 #[derive(serde::Deserialize)]
 pub struct FormData {
     email: String,
@@ -15,13 +16,13 @@ pub async fn subscribe(
     pool: web::Data<PgPool>,
 ) -> HttpResponse {
     let request_id = Uuid::new_v4();
-    log::info!(
+    tracing::info!(
         "request_id {} - Adding {} {} as a new subscriber",
         request_id,
         form.email,
         form.name
     );
-    log::info!("request_id {} - Saving new subscriber details in the database", request_id);
+    tracing::info!("request_id {} - Saving new subscriber details in the database", request_id);
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -36,11 +37,11 @@ pub async fn subscribe(
     .await
     {
         Ok(_) => {
-            log::info!("request_id {} - New subscriber details have been saved", request_id);
+            tracing::info!("request_id {} - New subscriber details have been saved", request_id);
             HttpResponse::Ok().finish()
         },
         Err(e) => {
-            log::error!("request_id {} - Failed to execute query: {:?}",request_id, e);
+            tracing::error!("request_id {} - Failed to execute query: {:?}",request_id, e);
             HttpResponse::InternalServerError().finish()
     }
     }
