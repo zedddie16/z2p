bd631b6db0baffbeb5c81851f4b0f5ce5785fecc
tuturuu <zedddiezxc@gmail.com>
2024-10-12 00:53:08 +0200

    moving span logging code to telemetry.rs file

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,35 @@ use std::net::TcpListener;
 use sqlx::{Connection, PgPool};
 use zero2prod::startup::run;
 use zero2prod::configuration::get_configuration;
-use tracing::subscriber::set_global_default;
-use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
-use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
-use tracing_log::LogTracer;
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
+
+/// Compose multiple layers into a `tracing`'s subscriber.
+///
+/// # Implementation Notes
+///
+/// We are using `impl Subscriber` as return type to avoid having to
+/// spell out the actual type of the returned subscriber, which is
+/// indeed quite complex.
+/// We need to explicitly call out that the returned subscriber is
+/// `Send` and `Sync` to make it possible to pass it to `init_subscriber`
+/// later on.
+
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    LogTracer::init().expect("Failed to set logger");
-
-    let env_filter = EnvFilter::try_from_default_env()
-        .unwrap_or_else(|_| EnvFilter::new("info"));
-    let formatting_layer = BunyanFormattingLayer::new(
-        "zero2prod".into(),
-        std::io::stdout
-    );
-    let subscriber = Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatting_layer);
-    set_global_default(subscriber).expect("Failed to set subscriber");
+    let subscriber = get_subscriber("zero2prod".into(), "info".into());
+    init_subscriber(subscriber);
+    // LogTracer::init().expect("Failed to set logger");
+    //
+    // let env_filter = EnvFilter::try_from_default_env()
+    //     .unwrap_or_else(|_| EnvFilter::new("info"));
+    // let formatting_layer = BunyanFormattingLayer::new(
+    //     "zero2prod".into(),
+    //     std::io::stdout
+    // );
+    // let subscriber = Registry::default()
+    //     .with(env_filter)
+    //     .with(JsonStorageLayer)
+    //     .with(formatting_layer);
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPool::connect(
             &configuration.database.connection_string()
