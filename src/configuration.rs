7a3e43f8c8ef3f360d2424a4648cddf512b68641
tuturuu <zedddiezxc@gmail.com>
2024-10-17 21:09:14 +0200

    app configuration for docker

--- src/configuration.rs
+++ src/configuration.rs
@@ -3,7 +3,12 @@ use secrecy::{ExposeSecret, Secret};
 #[derive(serde::Deserialize)]
 pub struct Settings {
     pub database: DatabaseSettings,
-    pub application_port: u16,
+    pub application: ApplicationSettings,
+}
+#[derive(serde::Deserialize)]
+pub struct ApplicationSettings {
+    pub port: u16,
+    pub host: String,
 }
 #[derive(serde::Deserialize)]
 pub struct DatabaseSettings {
