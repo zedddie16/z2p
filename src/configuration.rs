78954211b9e879e1aa3b608a8f574983fd57f2a6
tuturuu <zedddiezxc@gmail.com>
2024-10-19 16:07:22 +0200

    corrected error message

--- src/configuration.rs
+++ src/configuration.rs
@@ -27,20 +27,20 @@ pub struct DatabaseSettings {
 }
 
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
-    let base_path = std::env::current_dir().expect("Failed to determint the current directory");
+    let base_path = std::env::current_dir().expect("Failed to determine the current directory");
     let configuration_directory = base_path.join("configuration");
 
-    let enviroment: Environment = std::env::var("APP_ENVIRONMENT")
+    let environment: Environment = std::env::var("APP_ENVIRONMENT")
         .unwrap_or_else(|_| "local".into())
         .try_into()
         .expect("Failed to parse APP_ENVIROMENT");
-    let enviroment_filename = format!("{}.yaml", enviroment.as_str());
+    let environment_filename = format!("{}.yaml", environment.as_str());
     let settings = config::Config::builder()
         .add_source(config::File::from(
             configuration_directory.join("base.yaml"),
         ))
         .add_source(config::File::from(
-            configuration_directory.join(enviroment_filename),
+            configuration_directory.join(environment_filename),
         ))
         .add_source(
             config::Environment::with_prefix("APP")
