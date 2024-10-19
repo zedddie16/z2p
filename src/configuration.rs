785aed3dac518f2602425fc8792dd631f38431f6
tuturuu <zedddiezxc@gmail.com>
2024-10-19 13:52:05 +0200

    Updated tests and configuration to use new connection

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,6 +1,7 @@
 use secrecy::{ExposeSecret, Secret};
 use serde_aux::field_attributes::deserialize_number_from_string;
 use sqlx::postgres::PgConnectOptions;
+use sqlx::postgres::PgSslMode;
 
 #[derive(serde::Deserialize)]
 pub struct Settings {
