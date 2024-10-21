ef7c31c17f71a2dd0a06415641d4d2289f2424bf
tuturuu <zedddiezxc@gmail.com>
2024-10-21 22:06:12 +0200

    deleted unused import

--- src/configuration.rs
+++ src/configuration.rs
@@ -2,7 +2,6 @@ use secrecy::{ExposeSecret, Secret};
 use serde_aux::field_attributes::deserialize_number_from_string;
 use sqlx::postgres::PgConnectOptions;
 use sqlx::postgres::PgSslMode;
-use sqlx::ConnectOptions;
 
 #[derive(serde::Deserialize)]
 pub struct Settings {
@@ -88,7 +87,7 @@ impl DatabaseSettings {
         PgConnectOptions::new()
             .host(&self.host)
             .username(&self.username)
-            .password(&self.password.expose_secret())
+            .password(self.password.expose_secret())
             .port(self.port)
             .ssl_mode(ssl_mode)
     }
